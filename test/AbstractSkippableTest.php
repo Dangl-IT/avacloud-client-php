d9e12ce18972194099d3f7c0cae7327b8447111a
Thomas Helmrich <thomas@gigabit.de>
2022-06-13 13:44:59 +0200

    added abstract class to easily mark tests skippable

---
+++ test/AbstractSkippableTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dangl\AVACloud;
+
+class AbstractSkippableTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Setup before running any test cases
+     */
+    public static function setUpBeforeClass(): void
+    {
+    }
+
+    /**
+     * Setup before running each test case
+     */
+    public function setUp(): void
+    {
+        $this->markTestSkipped('Implementation of this test is not complete.');
+    }
+
+    /**
+     * Clean up after running each test case
+     */
+    public function tearDown(): void
+    {
+    }
+
+    /**
+     * Clean up after running all test cases
+     */
+    public static function tearDownAfterClass(): void
+    {
+    }
+}
